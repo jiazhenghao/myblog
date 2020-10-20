3d60599b3429740c1e4d170a58319d08a73eb7c8
Zhenghao Jia <cherie-xia@hotmail.com>
2020-10-20 23:31:05 +1100

    styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,48 @@
-export const plugins = [
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `pages`,
-      path: `${__dirname}/src/pages/`,
-    },
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `posts`,
-      path: `${__dirname}/src/posts/`,
-    },
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: `${__dirname}/src/images/`,
-    },
-  },
-  `gatsby-plugin-sharp`,
-  `gatsby-transformer-sharp`,
-  {
-    resolve: `gatsby-plugin-mdx`,
-    options: {
-      extensions: [`.mdx`, `.md`],
-    },
-    gatsbyRemarkPlugins: [
-      {
-        resolve: `gatsby-remark-images`,
-        options: {
-          maxWidth: 1200,
-        },
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
       },
-    ],
-  },
-  {
-    resolve: `gatsby-plugin-google-fonts`,
-    options: {
-      fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
-      display: "swap",
     },
-  },
-]
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+      gatsbyRemarkPlugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 1200,
+          },
+        },
+      ],
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
+        display: "swap",
+      },
+    },
+  ],
+}
